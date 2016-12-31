7caa17a588144caab07eda942f4785acd85e0030
Louis Rowan <louis.rowan@icloud.com>
2016-12-31 03:02:17 -0500

    adds shadow styling

--- app/containers/HomeContainer.js
+++ app/containers/HomeContainer.js
@@ -1,1 +1,24 @@
-HomeContainer.js
+const React = require('react')
+const Home = require('../components/Home')
+const Nav = require('../components/Nav')
+
+const HomeContainer = React.createClass({
+  getInitialState() {
+    return {
+      content: 'main'
+    }
+  },
+  handleClick(e) {
+    this.setState({ content: e })
+  },
+  render(){
+    return (
+      <div>
+        <Nav handleClick={this.handleClick} />
+        <Home content={this.state.content} />
+      </div>
+    )
+  }
+})
+
+module.exports = HomeContainer
