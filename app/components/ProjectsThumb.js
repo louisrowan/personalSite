cbda8488ea43f8f14cfc4d61e1045148c9d7def4
Louis Rowan <louis.rowan@icloud.com>
2017-01-01 19:33:52 -0500

    adds hover affect to zoom in on project thumb

--- app/components/ProjectsThumb.js
+++ app/components/ProjectsThumb.js
@@ -1,25 +1,50 @@
 const React = require('react')
 
 const ProjectsThumb = React.createClass({
+  getInitialState () {
+    return {
+      hover: false
+    }
+  },
+  mouseIn () {
+    this.setState({ hover: true })
+  },
+  mouseOut () {
+    this.setState({ hover: false })
+  },
   render(){
     const data = this.props.data
 
     const technologies = data.technologies.map((t, i) => {
-      return <li key={i}>{t}</li>
+      return <td key={i}>{t}</td>
     })
 
+    let hiddenDivClass;
+    let sectionClass;
+    this.state.hover ? hiddenDivClass = 'hiddenThumbDiv' : hiddenDivClass = 'hidden'
+    this.state.hover ? sectionClass = 'hoverProjectsThumb' : sectionClass = 'projectsThumb'
+
     return (
-      <section className='projectsThumb'>
+      <section
+      onMouseEnter={() => this.mouseIn()}
+      onMouseLeave ={() => this.mouseOut()}
+      className={sectionClass}>
+        <div className={hiddenDivClass}>
+          <h2 onClick={()=> this.props.expand()}>View Project</h2>
+        </div>
         <h1>{data.name}</h1>
         <p>{data.description}</p>
         <p onClick={() => this.props.expand() }>Show</p>
         <p>
           <a href={data.github}>Code</a>
         </p>
-        <h2>Technologies used:</h2>
-        <ul>
-          {technologies}
-        </ul>
+        <table>
+          <tbody>
+            <tr>
+            {technologies}
+            </tr>
+          </tbody>
+        </table>
         <div className='thumbImgDiv'>
           <img src={data.image} />
           <div></div>
