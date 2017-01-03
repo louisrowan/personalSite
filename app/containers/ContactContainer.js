d04bc4855a12fb89295746d54318ef98e3da5989
Louis Rowan <louis.rowan@icloud.com>
2017-01-03 00:17:47 -0500

    adds animation to form

--- app/containers/ContactContainer.js
+++ app/containers/ContactContainer.js
@@ -4,8 +4,10 @@ const $ = require('jquery')
 const ContactContainer = React.createClass({
   getInitialState () {
     return {
+      submitted: false,
       email: '',
-      message: ''
+      message: '',
+      phone: ''
     }
   },
   handleEmailChange (email) {
@@ -16,18 +18,25 @@ const ContactContainer = React.createClass({
     message = message.target.value
     this.setState({ message })
   },
+  handlePhoneChange (phone) {
+    phone = phone.target.value
+    this.setState({ phone })
+  },
   handleSubmit (e) {
     e.preventDefault()
     let email = this.state.email
     let message = this.state.message
+    let phone = this.state.phone
+    let that = this
     $.ajax({
       method: 'post',
       dataType: 'json',
       url: "http://formspree.io/louis.rowan@icloud.com",
-      data: { email: email, message: message }
+      data: { email: email, message: message, phone: phone }
     })
     .then((res) => {
       console.log(res)
+      that.setState({ submitted: true })
     })
     .catch((err) => {
       console.log(err)
@@ -35,15 +44,38 @@ const ContactContainer = React.createClass({
     })
   },
   render(){
+    console.log(this.state.submitted)
+    let formClass;
+    this.state.submitted ? formClass = 'formSubmit' : formClass = ''
+    let success;
+    if (this.state.submitted) {
+      success = <h1 className='formSuccess'>Success!</h1>
+    } else {
+      success = ''
+    }
     return (
       <div className='contactContainer'>
-        <h1>test</h1>
-        <form onSubmit={ (e) => this.handleSubmit(e)} >
-          <input onChange={(email)=> this.handleEmailChange(email)} type="email" name="email" placeholder="Your email" />
-          <textarea onChange={(message)=> this.handleMessageChange(message)} name="message" placeholder="Your message"></textarea>
-          <button type="submit">Send</button>
+        <h1>Contact Me</h1>
+        {success}
+        <form className={'contactForm ' + formClass} onSubmit={ (e) => this.handleSubmit(e)} >
+          <input className='contactInput'
+            onChange={(email)=> this.handleEmailChange(email)}
+            type="email"
+            name="email"
+            placeholder="Your email" />
+          <input className='contactInput'
+            onChange={(phone)=> this.handlePhoneChange(phone)}
+            type="text"
+            name="phone"
+            placeholder="Your phone number" />
+          <br />
+          <textarea className='contactTextarea'
+            onChange={(message)=> this.handleMessageChange(message)}
+            name="message"
+            placeholder="Your message"></textarea>
+          <br />
+          <button className='contactSubmit' type="submit">Submit</button>
         </form>
-        <h1>test</h1>
       </div>
     )
   }
