a65b62c8a57aa3c7b11439e2e98fc8e0931447fb
Louis Rowan <louis.rowan@icloud.com>
2017-01-04 00:40:39 -0500

    finally gets node server working correctly

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const path = require('path')
 const port = process.env.PORT || 8080
 const app = express()
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname + '/dist'))
+app.use(express.static(__dirname + '/public'))
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist/index.html'))
