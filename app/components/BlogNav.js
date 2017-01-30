b863d2890e198462e2e1e7f6f8e1cb775bf43c06
Louis Rowan <louis.rowan@icloud.com>
2017-01-30 15:03:08 -0500

    adds hidden nav

--- app/components/BlogNav.js
+++ app/components/BlogNav.js
@@ -1,10 +1,19 @@
 const React = require('react')
 const { Link } = require('react-router')
 const background = require('json!../../public/data.json').images.blogNav
+const posts = require('json!../../public/blog/blog.json').posts
 
 
 const BlogNav = React.createClass({
+  handleClick(){
+    var list = document.getElementById('blogDropdownList')
+    list.style.display='none';
+  },
   render(){
+    let listItems = posts.map((p) => {
+      var url = `/blog/${p.url}`
+      return <li key={p.id}><Link to={url}>{p.title}</Link></li>
+    })
     return (
       <nav id='blogNav'>
         <img id='blogNavGif' src={background} />
@@ -14,8 +23,14 @@ const BlogNav = React.createClass({
               <td>
                 <Link to='/'>Portfolio Site</Link>
               </td>
-              <td>
-                <Link to='/blog'>Posts Index</Link>
+              <td id='dropdownTD'>
+                <Link>Posts</Link>
+                <ul id='blogDropdownList' onClick={()=> this.handleClick()}>
+                  <li>
+                    <Link to='/blog'>Posts Index</Link>
+                  </li>
+                  {listItems}
+                </ul>
               </td>
             </tr>
           </tbody>
