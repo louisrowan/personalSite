f625abd69c397c31013aec3e88b7bbff3702073d
Louis Rowan <louis.rowan@icloud.com>
2017-01-29 21:48:16 -0500

    gives each page its own css file

--- app/containers/MainContainer.js
+++ app/containers/MainContainer.js
@@ -17,7 +17,7 @@ const MainContainer = React.createClass({
           My name is <span className='emp'>Louis Rowan</span> and I'm a self-taught <span className='emp'>Software Engineer</span>. I enjoy writing code that is performant, maintainable and useful. I learned <span className='emp'>Ruby</span>, <span className='emp'>Rails</span>, <span className='emp'>JavaScript</span>, <span className='emp'>Git/Github</span> and <span className='emp'>SQL</span> at DevBootcamp San Francisco, then taught myself <span className='emp'>D3</span>, <span className='emp'>C/C++</span>, <span className='emp'>React</span>, <span className='emp'>Node/Webpack/NPM</span> and <span className='emp'>Redux</span>.
         </p>
         <p className='mainParagraph'>
-          I graduated from the <span className='emp'>University of Connecticut</span> in 2013 with a BA in Economics, but more recently found a passion in the world of development. I am currently searching for the right opportunity, one where I can be a valued member of a team and be put into a position to continue learning and improving every day.
+          I graduated from the <span className='emp'>University of Connecticut</span> in 2013 with a <span className='emp'>BA in Economics</span>, but more recently found a passion in the world of development. I am currently searching for the right opportunity, one where I can be a valued member of a team and be put into a position to continue learning and improving every day.
         </p>
         <p className='mainParagraph'>
           I have started a <span className='emp'><Link to='/blog'>Blog </Link></span>as a place to journal some of the new ideas and concepts that I'm learning. My last entry was about using CSS animations and transitions in React, check it out!
